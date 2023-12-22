7ca63f00d84fc6edefe91946cc2c72d197b85aa3
siddheshraze <siddheshraze@gmail.com>
2023-12-21 23:12:38 -0500

    adding remaining CRUD commands to all other fixed data API routes

--- frontend/app/api/fixeddata/personnel/route.ts
+++ frontend/app/api/fixeddata/personnel/route.ts
@@ -1,6 +1,6 @@
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 import sql from "mssql";
-import {sqlConfig} from "@/config/macros";
+import {ErrorMessages, sqlConfig} from "@/config/macros";
 import {PersonnelRDS} from "@/config/sqlmacros";
 
 async function getSqlConnection(tries: number) {
@@ -26,7 +26,7 @@ export async function GET(): Promise<NextResponse<PersonnelRDS[]>> {
   let i = 0;
   let conn = await getSqlConnection(i);
   if (!conn) throw new Error('sql connection failed');
-  let results = await runQuery(conn, `SELECT * FROM forestgeo.Attributes`);
+  let results = await runQuery(conn, `SELECT * FROM forestgeo.Personnel`);
   if (!results) throw new Error("call failed");
   await conn.close();
   let personnelRows: PersonnelRDS[] = []
@@ -43,4 +43,71 @@ export async function GET(): Promise<NextResponse<PersonnelRDS[]>> {
     JSON.stringify(personnelRows),
     {status: 200}
   );
+}
+
+export async function POST(request: NextRequest) {
+  let i = 0;
+  let conn = await getSqlConnection(i);
+  if (!conn) throw new Error('sql connection failed');
+  const row: PersonnelRDS = {
+    id: 0,
+    personnelID: parseInt(request.nextUrl.searchParams.get('personnelID')!),
+    firstName: request.nextUrl.searchParams.get('firstName'),
+    lastName: request.nextUrl.searchParams.get('lastName'),
+    role: request.nextUrl.searchParams.get('role'),
+  }
+  
+  let checkPersonnelID = await runQuery(conn, `SELECT * FROM forestgeo.Personnel WHERE [PersonnelID] = ${row.personnelID}`);
+  if (!checkPersonnelID) return NextResponse.json({message: ErrorMessages.ICF}, {status: 400});
+  if (checkPersonnelID.recordset.length !== 0) return NextResponse.json({message: ErrorMessages.UKAE}, {status: 409});
+  let insertRow = await runQuery(conn, `INSERT INTO forestgeo.Personnel (PersonnelID, FirstName, LastName, Role) VALUES
+    (${row.personnelID}, '${row.firstName}', '${row.lastName}', '${row.role}')`);
+  if (!insertRow) return NextResponse.json({message: ErrorMessages.ICF}, {status: 400});
+  await conn.close();
+  return NextResponse.json({message: "Insert successful"}, {status: 200});
+}
+
+export async function PATCH(request: NextRequest) {
+  let i = 0;
+  let conn = await getSqlConnection(i);
+  if (!conn) throw new Error('sql connection failed');
+  
+  const oldPersonnelID = parseInt(request.nextUrl.searchParams.get('oldPersonnelID')!);
+  const row: PersonnelRDS = {
+    id: 0,
+    personnelID: parseInt(request.nextUrl.searchParams.get('personnelID')!),
+    firstName: request.nextUrl.searchParams.get('firstName')!,
+    lastName: request.nextUrl.searchParams.get('lastName')!,
+    role: request.nextUrl.searchParams.get('role')!,
+  };
+  
+  // check to ensure new code is not already taken
+  if (row.personnelID !== oldPersonnelID) { // if CODE is being updated, this check needs to happen
+    let newCodeCheck = await runQuery(conn, `SELECT * FROM forestgeo.Personnel WHERE [PersonnelID] = '${row.personnelID}'`);
+    if (!newCodeCheck) return NextResponse.json({message: ErrorMessages.SCF}, {status: 400});
+    if (newCodeCheck.recordset.length !== 0) return NextResponse.json({message: ErrorMessages.UKAE}, {status: 409});
+    
+    let results = await runQuery(conn,
+      `UPDATE forestgeo.Personnel SET [PersonnelID] = ${row.personnelID}, [FirstName] = '${row.firstName}', [LastName] = '${row.lastName}', [Role] = '${row.role}' WHERE [PersonnelID] = '${oldPersonnelID}'`);
+    if (!results) return NextResponse.json({message: ErrorMessages.UCF}, {status: 409});
+    await conn.close();
+    return NextResponse.json({message: "Update successful",}, {status: 200});
+  } else { // otherwise updating can focus solely on other columns
+    let results = await runQuery(conn, `UPDATE forestgeo.Personnel SET [FirstName] = '${row.firstName}', [LastName] = '${row.lastName}', [Role] = '${row.role}' WHERE [PersonnelID] = '${oldPersonnelID}'`);
+    if (!results) return NextResponse.json({message: ErrorMessages.UCF}, {status: 409});
+    await conn.close();
+    return NextResponse.json({message: "Update successful",}, {status: 200});
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  let i = 0;
+  let conn = await getSqlConnection(i);
+  if (!conn) throw new Error('sql connection failed');
+  
+  const deletePersonnelID = parseInt(request.nextUrl.searchParams.get('personnelID')!);
+  let deleteRow = await runQuery(conn, `DELETE FROM forestgeo.Personnel WHERE [PersonnelID] = ${deletePersonnelID}`);
+  if (!deleteRow) return NextResponse.json({message: ErrorMessages.DCF}, {status: 400});
+  await conn.close();
+  return NextResponse.json({message: "Delete successful",}, {status: 200});
 }
--- frontend/app/api/fixeddata/species/route.ts
+++ frontend/app/api/fixeddata/species/route.ts
@@ -1,6 +1,6 @@
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 import sql from "mssql";
-import {sqlConfig} from "@/config/macros";
+import {ErrorMessages, sqlConfig} from "@/config/macros";
 import {SpeciesRDS} from "@/config/sqlmacros";
 
 async function getSqlConnection(tries: number) {
@@ -49,4 +49,90 @@ export async function GET(): Promise<NextResponse<SpeciesRDS[]>> {
     JSON.stringify(speciesRows),
     {status: 200}
   );
+}
+
+export async function POST(request: NextRequest) {
+  let i = 0;
+  let conn = await getSqlConnection(i);
+  if (!conn) throw new Error('sql connection failed');
+  const row: SpeciesRDS = {
+    id: 0,
+    speciesID: parseInt(request.nextUrl.searchParams.get('speciesID')!),
+    genusID: request.nextUrl.searchParams.get('genusID') ? parseInt(request.nextUrl.searchParams.get('genusID')!) : null,
+    currentTaxonFlag: request.nextUrl.searchParams.get('currentTaxonFlag') ? JSON.parse(request.nextUrl.searchParams.get('currentTaxonFlag')!) : null,
+    obsoleteTaxonFlag: request.nextUrl.searchParams.get('obsoleteTaxonFlag') ? JSON.parse(request.nextUrl.searchParams.get('obsoleteTaxonFlag')!) : null,
+    speciesName: request.nextUrl.searchParams.get('speciesName'),
+    speciesCode: request.nextUrl.searchParams.get('speciesCode'),
+    idLevel: request.nextUrl.searchParams.get('idLevel'),
+    authority: request.nextUrl.searchParams.get('authority'),
+    fieldFamily: request.nextUrl.searchParams.get('fieldFamily'),
+    description: request.nextUrl.searchParams.get('description'),
+    referenceID: request.nextUrl.searchParams.get('referenceID') ? parseInt(request.nextUrl.searchParams.get('referenceID')!) : null,
+  }
+  let checkSpeciesID = await runQuery(conn, `SELECT * FROM forestgeo.Species WHERE [SpeciesID] = ${row.speciesID}`);
+  if (!checkSpeciesID) return NextResponse.json({message: ErrorMessages.ICF}, {status: 400});
+  if (checkSpeciesID.recordset.length !== 0) return NextResponse.json({message: ErrorMessages.UKAE}, {status: 409});
+  let insertRow = await runQuery(conn,
+    `INSERT INTO forestgeo.Species (SpeciesID, GenusID, CurrentTaxonFlag, ObsoleteTaxonFlag, SpeciesName, SpeciesCode,
+    IDLevel, Authority, FieldFamily, Description, ReferenceID) VALUES (${row.speciesID}, ${row.genusID}, '${row.currentTaxonFlag}',
+    '${row.obsoleteTaxonFlag}', '${row.speciesName}', '${row.speciesCode}', '${row.idLevel}', '${row.authority}', '${row.fieldFamily}', '${row.description}',
+    ${row.referenceID})`);
+  if (!insertRow) return NextResponse.json({message: ErrorMessages.ICF}, {status: 400});
+  await conn.close();
+  return NextResponse.json({message: "Insert successful"}, {status: 200});
+}
+
+export async function PATCH(request: NextRequest) {
+  let i = 0;
+  let conn = await getSqlConnection(i);
+  if (!conn) throw new Error('sql connection failed');
+  const oldSpeciesID = parseInt(request.nextUrl.searchParams.get('oldSpeciesID')!);
+  const row: SpeciesRDS = {
+    id: 0,
+    speciesID: parseInt(request.nextUrl.searchParams.get('speciesID')!),
+    genusID: request.nextUrl.searchParams.get('genusID') ? parseInt(request.nextUrl.searchParams.get('genusID')!) : null,
+    currentTaxonFlag: request.nextUrl.searchParams.get('currentTaxonFlag') ? JSON.parse(request.nextUrl.searchParams.get('currentTaxonFlag')!) : null,
+    obsoleteTaxonFlag: request.nextUrl.searchParams.get('obsoleteTaxonFlag') ? JSON.parse(request.nextUrl.searchParams.get('obsoleteTaxonFlag')!) : null,
+    speciesName: request.nextUrl.searchParams.get('speciesName'),
+    speciesCode: request.nextUrl.searchParams.get('speciesCode'),
+    idLevel: request.nextUrl.searchParams.get('idLevel'),
+    authority: request.nextUrl.searchParams.get('authority'),
+    fieldFamily: request.nextUrl.searchParams.get('fieldFamily'),
+    description: request.nextUrl.searchParams.get('description'),
+    referenceID: request.nextUrl.searchParams.get('referenceID') ? parseInt(request.nextUrl.searchParams.get('referenceID')!) : null,
+  }
+  
+  if (row.speciesID !== oldSpeciesID) {
+    let newSpeciesIDCheck = await runQuery(conn, `SELECT * FROM forestgeo.Species WHERE [SpeciesID] = '${row.speciesID}'`);
+    if (!newSpeciesIDCheck) return NextResponse.json({message: ErrorMessages.SCF}, {status: 400});
+    if (newSpeciesIDCheck.recordset.length !== 0) return NextResponse.json({message: ErrorMessages.UKAE}, {status: 409});
+    
+    let results = await runQuery(conn, `UPDATE forestgeo.Species
+    SET [SpeciesID] = ${row.speciesID}, [GenusID] = ${row.genusID}, [CurrentTaxonFlag] = '${row.currentTaxonFlag}',
+    [ObsoleteTaxonFlag] = '${row.obsoleteTaxonFlag}', [SpeciesName] = '${row.speciesName}', [SpeciesCode] = '${row.speciesCode}', [IDLevel] = '${row.idLevel}',
+    [Authority] = '${row.authority}', [FieldFamily] = '${row.fieldFamily}', [Description] = '${row.description}', [ReferenceID] = ${row.referenceID} WHERE [SpeciesID] = ${oldSpeciesID}`);
+    if (!results) return NextResponse.json({message: ErrorMessages.UCF}, {status: 409});
+    await conn.close();
+    return NextResponse.json({message: "Update successful",}, {status: 200});
+  } else {
+    let results = await runQuery(conn, `UPDATE forestgeo.Species
+    SET [GenusID] = ${row.genusID}, [CurrentTaxonFlag] = '${row.currentTaxonFlag}',
+    [ObsoleteTaxonFlag] = '${row.obsoleteTaxonFlag}', [SpeciesName] = '${row.speciesName}', [SpeciesCode] = '${row.speciesCode}', [IDLevel] = '${row.idLevel}',
+    [Authority] = '${row.authority}', [FieldFamily] = '${row.fieldFamily}', [Description] = '${row.description}', [ReferenceID] = ${row.referenceID} WHERE [SpeciesID] = ${oldSpeciesID}`);
+    if (!results) return NextResponse.json({message: ErrorMessages.UCF}, {status: 409});
+    await conn.close();
+    return NextResponse.json({message: "Update successful",}, {status: 200});
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  let i = 0;
+  let conn = await getSqlConnection(i);
+  if (!conn) throw new Error('sql connection failed');
+  
+  const deleteSpeciesID = parseInt(request.nextUrl.searchParams.get('speciesID')!);
+  let deleteRow = await runQuery(conn, `DELETE FROM forestgeo.SpeciesID WHERE [SpeciesID] = ${deleteSpeciesID}`);
+  if (!deleteRow) return NextResponse.json({message: ErrorMessages.DCF}, {status: 400});
+  await conn.close();
+  return NextResponse.json({message: "Delete successful",}, {status: 200});
 }
