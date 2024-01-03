26063f8bc67a2d93107febf3a9b74e5777cf622a
siddheshraze <siddheshraze@gmail.com>
2024-01-03 14:09:47 -0500

    saving changes --> divided uploaded file display into two displays, one for CSV and one for ArcGIS , additional changes to API structure to accommodate new schema and refit system to start accepting file uploads to specific tables instead of just single-type file uploads

--- frontend/app/(hub)/fileuploadhub/page.tsx
+++ frontend/app/(hub)/fileuploadhub/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import * as React from "react";
+import {FileTabView} from "@/components/fileupload/filehandling";
 
 // File Hub
 export default function Page() {
   return (
     <>
-      Scaffolding for Core Measurements View Hub
+      <FileTabView />
     </>
   );
 }
